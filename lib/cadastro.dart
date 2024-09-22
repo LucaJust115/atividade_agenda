8fb147757b15eea136c5ea702af910bdd8e6bec9
LucaJust115 <802.569@alunos.unigran.br>
2024-09-22 19:04:38 -0400

    inserida a validacao de cadastro de nome, telefone e email

--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'contato_repository.dart';
 
 class Cadastro extends StatefulWidget {
   final ContatoRepository contatos;
   final Contato? contato;
   final int? index;
+
   Cadastro({required this.contatos, this.contato, this.index});
 
   @override
@@ -15,6 +17,7 @@ class _CadastroState extends State<Cadastro> {
   final TextEditingController nomeController = TextEditingController();
   final TextEditingController telefoneController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   final ContatoRepository contatos;
   final Contato? contato;
   final int? index;
@@ -31,6 +34,30 @@ class _CadastroState extends State<Cadastro> {
     }
   }
 
+  String? _validarNome(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'O nome não pode estar vazio'; // validação para o campo nome
+    }
+    return null;
+  }
+
+  String? _validarTelefone(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'O telefone não pode estar vazio';
+    }
+    if (value.length != 11) {
+      return 'O telefone deve ter 11 dígitos'; // verifica se a quantidade de dígitos está correta
+    }
+    return null;
+  }
+
+  String? _validarEmail(String? value) {
+    if (value == null || !value.contains('@')) { // verifica se e um endereco de email com '@'
+      return 'Email inválido. Deve conter @';
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,45 +66,59 @@ class _CadastroState extends State<Cadastro> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: 'Entre com nome'),
-              controller: nomeController,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Entre com telefone'),
-              controller: telefoneController,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Entre com email'),
-              controller: emailController,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  if (contato == null) {
-                    contatos.addContato(Contato(
-                        nome: nomeController.text,
-                        telefone: telefoneController.text,
-                        email: emailController.text));
-                  } else {
-                    contatos.updateContato(
-                      index!,
-                      Contato(
-                        nome: nomeController.text,
-                        telefone: telefoneController.text,
-                        email: emailController.text,
-                      ),
-                    );
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Nome'),
+                controller: nomeController,
+                validator: _validarNome, // chama a funcao de validacao
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Telefone (somente números)'),
+                controller: telefoneController,
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly, // permitir apenas números (TENTEI USAR O REGEX MAS FRACASSEI E NAO PRESTOU DE FORMA ALGUMA)
+                ],
+                validator: _validarTelefone,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                validator: _validarEmail,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    setState(() {
+                      if (contato == null) {
+                        contatos.addContato(Contato(
+                          nome: nomeController.text,
+                          telefone: telefoneController.text,
+                          email: emailController.text,
+                        ));
+                      } else {
+                        contatos.updateContato(
+                          index!,
+                          Contato(
+                            nome: nomeController.text,
+                            telefone: telefoneController.text,
+                            email: emailController.text,
+                          ),
+                        );
+                      }
+                    });
+                    Navigator.pop(context);
                   }
-                });
-                Navigator.pop(context);
-              },
-              child: Text(contato == null ? 'Salvar' : 'Editar'),
-            ),
-          ],
+                },
+                child: Text(contato == null ? 'Salvar' : 'Editar'),
+              ),
+            ],
+          ),
         ),
       ),
     );
